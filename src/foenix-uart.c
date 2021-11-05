64a2ded6e1122f5d703ce9679f28b8430791f8ed
Håkan Thörngren <hth313@gmail.com>
2021-11-05 13:26:29 -0700

    Update UART code for C256/U
    Add speed table setting for it as it differs from systems with
    SuperIO. Also write the speed properly using a 16 bit word.

--- src/foenix-uart.c
+++ src/foenix-uart.c
@@ -5,23 +5,40 @@
 extern void breakHandler();
 extern void uartInterrupt();
 
-#ifdef SYS_C256_FMX
+#ifdef __TARGET_C256_FMX__
 /* Base address for UART 1 (COM1) */
 #define UART_BASE 0xaf13f8
 /* Base address for UART 2 (COM2) */
-//#define UART2_BASE 0xaf12f8
+#define UART2_BASE 0xaf12f8
+#define UART_300      384
+#define UART_1200      96
+#define UART_2400      48
+#define UART_4800      24
 #define UART_9600      12
 #define UART_38400      3
 #define UART_115200     1
 #else
 /* Base address for UART 1 (COM1) in the C256 Foenix U (only 1 Serial port) */
 #define UART_BASE 0xaf18f8
-//#define UART2_BASE 0xaf18f8
-#define UART_115200     8
+#define UART2_BASE UART_BASE
+
+// The UART in the C256/U uses the CPU clock (14.318MHz).
+// While not an ideal clock for standard RS232 speeds,
+//it is serviceable
+#define UART_300   2983
+#define UART_600   1491
+#define UART_1200   746
+#define UART_2400   373
+#define UART_4800   186
+#define UART_9600    93
+#define UART_19200   47
+#define UART_38400   23
+#define UART_57600   16
+#define UART_115200   8
 #endif
 
 #define UART_TRHB *(volatile __far char*)(UART_BASE + 0)    // Transmit/Receive Hold Buffer
-#define UART_DLL  UART_TRHB
+#define UART_DL   *(volatile __far unsigned short*)(UART_BASE + 0)
 #define UART_IER  *(volatile __far char*)(UART_BASE + 1)    // Interupt Enable Register
 #define UART_FCR  *(volatile __far char*)(UART_BASE + 2)    // FIFO Control Register
 #define UART_LCR  *(volatile __far char*)(UART_BASE + 3)    // Line Control Register
@@ -59,22 +76,22 @@ void initialize(void)
   origBRKVector = BRK_VECTOR;
 
   // Insert our own BRK vector
-  BRK_VECTOR = breakHandler;
-
+//  BRK_VECTOR = breakHandler;
 
   // Set speed
   UART_LCR |= LCR_DLB;     // enable divisor latch
-  UART_DLL = UART_115200;
+  UART_DL = UART_115200;
   UART_LCR &= ~LCR_DLB;    // disable divisor latch
 
   // Set mode
   UART_LCR = LCR_PARITY_NONE | LCR_STOPBIT_1 | LCR_DATABITS_8;
 
-  UART_FCR = 0b11100001; // enable FIFO
+//  UART_FCR = 0b11100001; // enable FIFO
+  UART_FCR = 0b10000111; // enable FIFO
 
   // Enable interrupt when receiving data
 //  UART_IER = UINT_DATA_AVAIL;
-  UART_IER = 0;   // polled mode
+  UART_IER = 0;   // polled mode used when in control
 }
 
 char getDebugChar(void)
