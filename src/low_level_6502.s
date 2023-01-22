c21d14b05a7ad2718def8c60780f8c2b12f69626
Håkan Thörngren <hth313@gmail.com>
2023-01-22 12:55:46 -0800

    6502: Refactor a bit towards F256 Jr

--- src/low_level_6502.s
+++ src/low_level_6502.s
@@ -1,14 +1,27 @@
               .public interruptHandler
-              .public breakHandler, interruptHandler
+              .public interruptHandler
               .public continueExecution
               .extern _Zp, registers, handleException, origIRQVector
 
-#define ACIA 0xde00
+#ifdef ACIA_6551
+ACIA:         .equ    0xde00
+#endif
 
+#ifdef FOENIX_JR
+UART_BASE:    .equ    0xd630
+
+UART_TRHB:    .equ    UART_BASE
+UART_LSR:     .equ    (UART_BASE + 5)
+
+INT_PENDING_REG1: .equ 0x000141
+FNX1_INT04_COM1: .equ 0x10
+
+#endif
               .section code
 ;;; * Break instruction is the software breakpoint
 breakHandler:                       ; save to 'registers'
               jsr     saveFrame
+              tax                   ; X= actual stack pointer
               sec                   ; adjust stop address for BRK
               lda     registers + 0
               sbc     #2
@@ -17,45 +30,78 @@ breakHandler:                       ; save to 'registers'
               dec     registers + 1
 10$:          lda     #19           ; stop signal
 toMonitor:    sta     zp:_Zp+0
+#ifdef __CALYPSI_CORE_65C02__
+              stz     zp:_Zp+1
+#else
               lda     #0
               sta     zp:_Zp+1
+#endif
+              txs                   ; set correct stack
               jmp     handleException
 
 ;;; * Check for ACIA control C, otherwise call old handler
 interruptHandler:
+              pha
+#ifdef __CALYPSI_CORE_65C02__
+              phx
+#else
+              txa
+              phx
+#endif
+              tsx
+              lda     0x103,x
+              and     #0x10
+              bne     breakHandler
+
+#ifdef ACIA_6551
               lda     ACIA + 1
               and     #8            ; got a character?
               beq     10$           ; no
               lda     ACIA          ; read character
+#endif
+#ifdef FOENIX_JR
+              lda     UART_LSR
+              lsr     a             ; data available?
+              bcc     10$           ; no
+              lda     UART_TRHB     ; read character
+              phx
+              ldx     #FNX1_INT04_COM1
+              stx     INT_PENDING_REG1
+              plx
+#endif
               cmp     #3            ; Ctrl-C?
               bne     10$           ; no
+              tsx
               jsr     saveFrame     ; yes, save registers and stop execution
+              tax                   ; X= correct stack pointer
               lda     #2            ; INT (interrupt by Ctrl-C) signal
-              bne     toMonitor
-10$:          jmp     (origIRQVector) ; let next in chain handle it
+              bne     toMonitor     ; always jump
 
-saveFrame:    txa                   ; A=sp
+10$:
+#ifdef __CALYPSI_CORE_65C02__
+              plx                   ; restore A and X
+#else
+              pla
+              tax
+#endif
+              pla
+              jmp     (origIRQVector) ; let next in chain handle it
+
+saveFrame:    lda     0x101,x       ; X
+              sta     registers + 3
+              lda     0x102,x       ; A
+              sta     registers + 2
+              lda     0x103,x       ; status register
+              sta     registers + 6
+              lda     0x104,x       ; save PC
+              sta     registers + 0
+              lda     0x105,x
+              sta     registers + 1
               cld
               clc
-              adc     #6            ; A=actual SP
-              sta     registers + 5 ; save SP
-
-              lda     0x104,x       ; save status register
-              sta     registers + 6
-
-              lda     0x101,x       ; save Y
-              sta     registers + 4
-
-              lda     0x102,x       ; save X
-              sta     registers + 3
-
-              lda     0x103,x       ; save A
-              sta     registers + 2
-
-              lda     0x105,x       ; save PC
-              sta     registers + 0
-              lda     0x106,x
-              sta     registers + 1
+              txa
+              adc     #7
+              sta     registers + 5 ; stack pointer
               rts
 
 continueExecution:
